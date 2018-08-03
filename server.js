3346ca17d370a0eaa93dca135bbcf538e8429e32
K Spivak <k.spivak@advcash.com>
2018-08-03 13:32:47 +0300

    node

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ var io = require('socket.io')(server);
 io.on('connection', function (client) {
     console.log('have a connection');
 
-    client.on('node-ready', function (data) {
+    io.on('node-ready', function (data) {
         console.log(`node-ready: ${data}`);
     });
-    client.on('disconnect', function () { });
+    io.on('disconnect', function () { });
 });
 
 
-client.on('live-stream', function (data) {
-    client.emit('all-node-info', 'hello world');
-    client.emit('node', data);
+io.on('live-stream', function (data) {
+    io.emit('all-node-info', 'hello world');
+    io.emit('node', data);
     console.log(`it is base64 !video!: ${data}`);
 });
 
