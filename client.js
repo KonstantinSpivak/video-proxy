23c7ad479ddf350d7a357dc1e13a48fd448680e0
K.Spivak <k.spivak@advcash.com>
2019-11-11 11:09:03 +0200

    fix: disconnect

--- client.js
+++ client.js
@@ -2,14 +2,14 @@ const socket = require('socket.io-client')('http://35.242.194.228:3000');
 const v4l2camera = require('v4l2camera');
 
 let isVideoStream = false;
-socket.on('connect', function() {
+socket.on('connect', function(e) {
   isVideoStream = true;
-  console.log('Connect');
+  console.log('Connect:', e);
 });
 
-socket.on('disconect', function() {
+socket.on('disconnect', function() {
   isVideoStream = false;
-  console.log('disconect');
+  console.log('disconnect');
 });
 
 socket.on('error', function(e) {
--- server.js
+++ server.js
@@ -4,17 +4,16 @@ const ioReciver = require('socket.io')(serverReciver);
 const serverSender = require('http').createServer();
 const ioSender = require('socket.io')(serverSender);
 
-ioReciver.on('connection', function (client) {
-    //console.log('have a connection');
-    client.on('live-stream', function (data) {
-        ioSender.compress(true).emit('node', data);
-    });
-    client.on('node-ready', function (data) {
-        //console.log(`node-ready: ${data}`);
-    });
-    client.on('disconnect', function () { });
-});
+ioReciver.on('connection', function(client) {
+  console.log('#node_streamer_connected');
+  client.on('live-stream', function(data) {
+    // ioSender.compress(true).emit('node', data);
+  });
 
+  client.on('disconnect', function() {
+    console.log('#node_streamer_disconected');
+  });
+});
 
 serverReciver.listen(3000);
 serverSender.listen(3001);
